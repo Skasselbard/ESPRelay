f8b08e5f3d6d7fb65dbdd9315762879d57c3fea7
Tom Meyer <tom.meyer@uni-rostock.de>
2018-11-11 18:22:13 +0100

    updated deploy script

--- deployTools/deploy.py
+++ deployTools/deploy.py
@@ -42,7 +42,7 @@ def main(argv):
     httpd = socketserver.TCPServer(("", PORT), Handler)
 
     server_thread = threading.Thread(target=httpd.serve_forever)
-    server_thread.daemon = False
+    server_thread.daemon = True
     server_thread.start()
 
     # do work
@@ -55,13 +55,15 @@ def main(argv):
         if p.isfile(p.join(path, f)) and p.splitext(f)[1] == ".lua"
     ]
     # transmit files
-    # for f in files:
-    #     filepath = path+'/'+f
-    #     print("transmitting: " + filepath)
-    #     command = "loadFile(\"+192.168.34\", \"/"+f+"\", \""+f+"\")"
-    #     s.sendall(str.encode(command))
-    #     s.recv(1024)
-    #     time.sleep(1)
+    serverIp = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(
+        ("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
+    for f in files:
+        filepath = path+'/'+f
+        print("transmitting: " + filepath)
+        command = "loadFile(\""+serverIp+"\", \"/"+f+"\", \""+f+"\")\n"
+        s.sendall(str.encode(command))
+        time.sleep(2)
+        s.recv(1024)
     s.close()
 
 
