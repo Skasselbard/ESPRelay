543ac28abb5125d1f55827c01fb7c1ed354e9a1d
Tom Meyer <tom.meyer@uni-rostock.de>
2018-11-15 20:45:51 +0100

    dont wait for answers in console cmd mode

--- deployTools/console.py
+++ deployTools/console.py
@@ -6,53 +6,56 @@ import readline
 import cmd
 
 
-def printHelp():
-    print("run with: \nconsole -d device -b baudrate")
-
-
-# parse arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("device", help="UART target device")
-parser.add_argument("baudRate", nargs='?', const=115200)
-parser.add_argument("-c", nargs='?', const=115200, help="single command")
-args = parser.parse_args()
-if args.baudRate == None:
-    args.baudRate = 115200
-print("device: " + args.device)
-print("baudRate: " + str(args.baudRate))
-#################
-
-s = serial.Serial(args.device)
-s.baudrate = args.baudRate
-
-if args.c != None:
-    line = args.c+"\r\n"
+def executeCommand(device, baudRate, command):
+    s = serial.Serial(device)
+    s.baudrate = baudRate
+    line = command+"\r\n"
     s.write(line.encode())
     s.readline()
-    print("Answer: " + s.readline().decode('ascii'))
-    exit(0)
 
 
-class Console(cmd.Cmd):
-    intro = 'Hi'
-    prompt = '>'
-    file = None
+def main():
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("device", help="UART target device")
+    parser.add_argument("baudRate", nargs='?', const=115200)
+    parser.add_argument("-c", nargs='?', help="single command")
+    args = parser.parse_args()
+    if args.baudRate == None:
+        args.baudRate = 115200
 
-    def default(self, line):
-        line = line+"\r\n"
-        s.write(line.encode())
+    # run single command and exit if requested
+    if args.c != None:
+        executeCommand(args.device, args.baudRate, args.c)
+        exit(0)
+
+    # init serial
+    s = serial.Serial(args.device)
+    s.baudrate = args.baudRate
+
+    # init "console"
+
+    class Console(cmd.Cmd):
+        intro = 'Hi'
+        prompt = '>'
+        file = None
+
+        def default(self, line):
+            line = line+"\r\n"
+            s.write(line.encode())
+
+    def readNode():
+        while True:
+            sys.stdout.write(s.read(1).decode('ascii'))
+
+    # run reading thread
+    readThread = threading.Thread(target=readNode)
+    readThread.daemon = True
+    readThread.start()
+
+    # start
+    Console().cmdloop()
 
 
-def readNode():
-    while True:
-        sys.stdout.write(s.read(1).decode('ascii'))
-
-
-readThread = threading.Thread(target=readNode)
-readThread.daemon = True
-readThread.start()
-
-Console().cmdloop()
-# while True:
-#     line = input(">")
-#     s.write(line.encode())
+if __name__ == "__main__":
+    main()
