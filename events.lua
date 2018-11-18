193c02557a2f3054b7ddda5f603736acffe35b81
Tom Meyer <tom.meyer@uni-rostock.de>
2018-11-18 13:13:25 +0100

    first light controller implementation

--- events.lua
+++ events.lua
@@ -5,16 +5,50 @@
 -- add your subscription to the others in the "connect" event and
 -- add your logic to the message event
 
+location = "wohnzimmer/"
+purpose = "licht/"
+
+pwm.setup(1, 220, 512)
+pwm.setup(2, 220, 512)
+pwm.setup(3, 220, 512)
+pwm.setup(4, 220, 512)
+pwm.start(1)
+pwm.start(2)
+pwm.start(3)
+pwm.start(4)
+
+function led(g, r, b, w)
+    pwm.setduty(1, g)
+    pwm.setduty(2, r)
+    pwm.setduty(3, b)
+    pwm.setduty(4, w)
+    print("set light to:")
+    print("red: "..r)
+    print("green: "..g)
+    print("blue: "..b)
+    print("white: "..w)
+    local message = "{"..r..","..g..","..b..","..w.."}"
+    if client ~= nil then
+        client:publish(
+            "status/"..location..purpose..mqttName,
+            message,
+            0,
+            0
+        )
+    end
+end
+led(0, 0, 0, 0)
+
 mqttClient:on("connect", function(c) 
     mqttTimer:stop()
     print("mqtt connected as: "..mqttName)
     client = c
-    client:publish("status/ip/"..mqttName, wifi.sta.getip(),0,0)
+    client:publish("status/"..location..purpose..mqttName.."/ip", wifi.sta.getip(),0,0)
     -- add your subscription here
     client:subscribe({
-      ["control/"..mqttName] = 0,
+      ["control/"..location..purpose..mqttName] = 0,
       ["control/all"] = 0,
-      ["#"] = 0
+      ["status/"..location..purpose..mqttName] = 0
     })
 end)
 
@@ -24,7 +58,7 @@ mqttClient:on("offline", function(c)
 end)
 
 mqttClient:on("message", function(client, topic, message)
-    if topic == "control/"..mqttName or topic == "control/all" then
+    if topic == "control/"..location..purpose..mqttName or topic == "control/all" then
         node.input(message)
     else
       print(topic .. ":" ) 
--- loop.lua
+++ loop.lua
@@ -3,12 +3,6 @@
 counter = 1
 local loop = {}
 loop.gas = function()
-    line = ""
-    for i = 1, counter +1 do 
-        line = line.."la"
-    end
-    print(line)
-    counter = (counter +1)%5
 end
 
 return loop
